273976becd38b51683b109676d65e7ba702bdf63
Wiktor P <wiktor.poprawski@edu.uekat.pl>
2023-01-17 21:51:17 +0100

    password hashing salt is not accountCreationDate

--- BasiaProjektRazorPages/Pages/Account/Register.cshtml.cs
+++ BasiaProjektRazorPages/Pages/Account/Register.cshtml.cs
@@ -80,10 +80,11 @@ namespace BasiaProjektRazorPages.Pages.Account
             if (ok)
             {
                 using (IDbConnection conn = DbHelper.GetDbConnection())
-                {                    
-                    string passwordHash = AccountHelper.hashPassword(password, userName);
+                {
+                    DateTime creation = DateTime.Now;
+                    string passwordHash = AccountHelper.hashPassword(password, creation.ToString("s"));
                     int clientId = this.createEmptyClientDbEntry(); // <- not finished
-                    var result = conn.Query($"INSERT INTO Konto (ID_Klienta, LoginUzytkownika, Email, HashHasla) VALUES({clientId}, '{userName}', '{email}', '{passwordHash}')");
+                    var result = conn.Query($"INSERT INTO Konto (ID_Klienta, LoginUzytkownika, Email, HashHasla, DataUtworzenia) VALUES({clientId}, '{userName}', '{email}', '{passwordHash}', '{creation.ToString("s")}')");
                     alertClass = "alert-success";
                     alertValue = "Pomyślnie utworzono!\nNa podany email został posłany link aktywacyjny.";
                     accountCreated = true;
@@ -98,7 +99,7 @@ namespace BasiaProjektRazorPages.Pages.Account
             {
                 int biggestClientId = conn.ExecuteScalar<int>("SELECT TOP 1 ID_Klienta FROM Klient ORDER BY ID_Klienta DESC");
                 biggestClientId++;
-                int clientId = conn.ExecuteScalar<int>($"INSERT INTO Klient VALUES(NULL, NULL, NULL); SELECT SCOPE_IDENTITY();");
+                int clientId = conn.ExecuteScalar<int>($"INSERT INTO Klient VALUES(NULL, NULL, NULL, NULL); SELECT SCOPE_IDENTITY();");
                 return clientId;
             }
         }
