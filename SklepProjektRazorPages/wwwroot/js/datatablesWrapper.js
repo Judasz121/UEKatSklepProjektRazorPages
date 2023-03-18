7dd6ad5ee3bc00cbf40a5824e95cc8018fc580ae
Wiktor P <wiktor.poprawski@edu.uekat.pl>
2023-03-18 13:40:10 +0100

    moved GenerateDataTables from site.js to datatablesWrapper.js renamed GenerateDataTables to GenerateDataTable

---
+++ SklepProjektRazorPages/wwwroot/js/datatablesWrapper.js
@@ -0,0 +1,31 @@
+﻿function GenerateDataTable($tableEl, customDatatableOptions) {
+    $tableEl = $tableEl instanceof jQuery ? $tableEl : $($tableEl);
+    let dataTableOptions = {
+
+
+        // search inputs on column headers
+        dom: "ltipr",
+        searching: true,
+        initComplete: function (settings) {
+            
+            this.api()
+                .columns()
+                .every(function () {
+                    $("selector").click(false);
+                    var that = this;
+                    let $input = $("input", this.header());
+
+                    $input.click(false); // prevent sorting when clicked
+                    $input.on("keyup change clear ", function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                });
+
+        },
+        ...customDatatableOptions
+    };
+
+    return $tableEl.DataTable(dataTableOptions);
+};
--- SklepProjektRazorPages/wwwroot/js/site.js
+++ SklepProjektRazorPages/wwwroot/js/site.js
@@ -104,33 +104,6 @@ window.onclick = function (event) {
         }
     }
 }
-function GenerateDataTables($tableEl) {
-    $tableEl = $tableEl instanceof jQuery ? $tableEl : $($tableEl);
-    let dataTableOptions = {
-        dom: "ltipr",
-        searching: true,
-        initComplete: function (settings) {
-            //console.log("initComplete");
-            this.api()
-                .columns()
-                .every(function () {
-                    $("selector").click(false);
-                    var that = this;
-                    let $input = $("input", this.header());
-
-                    $input.click(false); // prevent sorting when clicked
-                    $input.on("keyup change clear ", function () {
-                        if (that.search() !== this.value) {
-                            that.search(this.value).draw();
-                        }
-                    });
-                });
-
-        },
-    };
-
-    return $tableEl.DataTable(dataTableOptions);
-};
 
 // Validate if user is logged
 function CheckifLogged(argument) {
