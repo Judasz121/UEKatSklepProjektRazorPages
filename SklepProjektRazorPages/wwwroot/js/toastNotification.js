a4630346613e90811596f754fbcfed43a011d43a
Wiktor P <wiktor.poprawski@edu.uekat.pl>
2023-03-19 17:24:30 +0100

    toast style = "danger" argument adjustment

--- SklepProjektRazorPages/wwwroot/js/toastNotification.js
+++ SklepProjektRazorPages/wwwroot/js/toastNotification.js
@@ -2,6 +2,8 @@
 // this file and function's sole purpose is to provide compatibility in case of changing the toast notification library
 
 function showToast(title, message, style = "info") {
+    if (style == "danger")
+        style = "error";
     iziToast[style]({
         title: title,
         message: message
