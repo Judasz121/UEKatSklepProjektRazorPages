0d993051662b1426c183f737d04af75409c93245
Wiktor P <wiktor.poprawski@edu.uekat.pl>
2023-03-19 17:00:40 +0100

    added iziToast

--- SklepProjektRazorPages/wwwroot/js/toastNotification.js
+++ SklepProjektRazorPages/wwwroot/js/toastNotification.js
@@ -1,6 +1,9 @@
-﻿function showToast(title, message, style = "info") {
+﻿
+// this file and function's sole purpose is to provide compatibility in case of changing the toast notification library
+
+function showToast(title, message, style = "info") {
     iziToast[style]({
         title: title,
         message: message
-    })
+    });
 }
